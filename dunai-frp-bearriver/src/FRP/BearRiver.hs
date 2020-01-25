4d20ce7f7a70838432cedde2ec2fe9d26ad30d13
Alex Stuart <alex@das.li>
2020-01-25 12:22:03 -0800

    Added `arrTime` to Bear River

--- dunai-frp-bearriver/src/FRP/BearRiver.hs
+++ dunai-frp-bearriver/src/FRP/BearRiver.hs
@@ -32,8 +32,9 @@ import           Control.Arrow                                  as X
 import qualified Control.Category                               as Category
 import           Control.Monad                                  (mapM)
 import           Control.Monad.Random
+import           Control.Monad.Reader                           (reader)
 import           Control.Monad.Trans.Maybe
-import           Control.Monad.Trans.MSF                        hiding (link, switch)
+import           Control.Monad.Trans.MSF                        hiding (link, reader, switch)
 import qualified Control.Monad.Trans.MSF                        as MSF
 import           Control.Monad.Trans.MSF.Except                 as MSF hiding
                                                                         (link, switch)
@@ -666,3 +667,15 @@ evalAt sf dt a = runIdentity $ runReaderT (unMSF sf a) dt
 -- discrete and step based.
 evalFuture :: SF Identity a b -> a -> DTime -> (b, SF Identity a b)
 evalFuture sf = flip (evalAt sf)
+
+-- * Auxiliary functions
+
+-- ** Event handling
+replaceOnce :: Monad m => a -> SF m a a
+replaceOnce a = dSwitch (arr $ const (a, Event ())) (const $ arr id)
+
+-- ** Tuples
+dup  x     = (x,x)
+
+arrTime :: Monad m => (DTime -> a -> b) -> SF m a b
+arrTime f = arrM (\a -> reader f >>= \fdt -> return (fdt a))
